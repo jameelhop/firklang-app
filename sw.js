0337935f8b6d59004b42291ac8b805c2298565ec
Jameel Hopland <jameel@hopland.dk>
2026-08-31 16:14:06 +0200

    Hjem: smalt hormonbaand og dagens kort

--- sw.js
+++ sw.js
@@ -17,7 +17,7 @@
 // bundtets hash. Aendrer den sig ikke, opdager browseren aldrig at der findes
 // en ny service worker - det var praecis fejlen der pinnede brugere til
 // den version de foerst hentede.
-const CACHE_VERSION = "firklang-3a6f2c8c4efc";
+const CACHE_VERSION = "firklang-3e15bfde4f5f";
 
 const SKAL = [
   "./",
