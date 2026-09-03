7131c0713a19ec25b2a342e564414f11bbe588cf
Jameel Hopland <jameel@hopland.dk>
2026-09-03 23:57:07 +0200

    Planen holder ved genaabning

--- sw.js
+++ sw.js
@@ -17,7 +17,7 @@
 // bundtets hash. Aendrer den sig ikke, opdager browseren aldrig at der findes
 // en ny service worker - det var praecis fejlen der pinnede brugere til
 // den version de foerst hentede.
-const CACHE_VERSION = "firklang-df5185bb515a";
+const CACHE_VERSION = "firklang-1b4d1b125ee2";
 
 const SKAL = [
   "./",
