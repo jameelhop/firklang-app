1cccf2c13216678a4c8e72695a905766f1e26516
Jameel Hopland <jameel@hopland.dk>
2026-09-04 06:19:01 +0200

    Kalibreringen kan ses med det samme

--- sw.js
+++ sw.js
@@ -17,7 +17,7 @@
 // bundtets hash. Aendrer den sig ikke, opdager browseren aldrig at der findes
 // en ny service worker - det var praecis fejlen der pinnede brugere til
 // den version de foerst hentede.
-const CACHE_VERSION = "firklang-3f4920c7357d";
+const CACHE_VERSION = "firklang-79001b3c3ae2";
 
 const SKAL = [
   "./",
