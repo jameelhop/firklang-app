b35966be666126f6da4ddfe41faa223d70dffa84
Jameel Hopland <jameel@hopland.dk>
2026-09-04 06:39:05 +0200

    Hjemmeskaerms-hint

--- sw.js
+++ sw.js
@@ -17,7 +17,7 @@
 // bundtets hash. Aendrer den sig ikke, opdager browseren aldrig at der findes
 // en ny service worker - det var praecis fejlen der pinnede brugere til
 // den version de foerst hentede.
-const CACHE_VERSION = "firklang-5ed8d3c547d0";
+const CACHE_VERSION = "firklang-9b14fbb16e97";
 
 const SKAL = [
   "./",
